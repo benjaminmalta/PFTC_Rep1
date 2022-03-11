b9919eef3bf8cdf6766d9d26dc03d1d6b8a52e58
benjaminmalta <56227477+benjaminmalta@users.noreply.github.com>
2022-03-11 09:57:02 +0100

    Updated

--- backend/db.js
+++ backend/db.js
@@ -1,30 +1,39 @@
-export const GOOGLE_APPLICATION_CREDENTIALS= "./key.json";
 import Firestore from "@google-cloud/firestore";
+import { createHmac } from "crypto";
 
-const Firestore = require('@google-cloud/firestore');
+//Google Cloud key
+export const GOOGLE_APPLICATION_CREDENTIALS = "./key.json";
 
+//Instantiating Firestore with project details
 const db = new Firestore({
-  projectId: 'pftc-msd-0000001',
+  projectId: "pftc0000001",
   keyFilename: GOOGLE_APPLICATION_CREDENTIALS,
 });
 
 //Collection (Table)
 //Document (Row)
-//docRef selects the collection and corresponding document
-
-
-
-export async function CreateUser(name, surname, email, password){
-  const docRef = db.collection("users").doc(email);  
+//docRef selects the collection
+export async function CreateUser(name, surname, email, password) {
+  const docRef = db.collection("users").doc();
   return await docRef.set({
-        name: name,
-        surname: surname,
-        email: email,
-        password: password
-    });
+    name: name,
+    surname: surname,
+    email: email,
+    password: HashPassword(password),
+  });
 }
 
-export async function GetUser(email){  
-  const docRef =  db.collection('users').doc(email);
-  return await docRef.get();
+export async function GetUser(email) {
+  const docRef = db.collection("users");
+  const snapshot = await docRef.where("email", "==", email).get();
+  let data = [];
+  snapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+  return data;
+}
+
+export function HashPassword(password) {
+  const secret = "i<3PfC";
+  return createHmac("sha256", password).update(secret).digest("hex");
 }
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,20 @@ import Express from "express";
 import cors from "cors";
 import { v4 as uuid } from "uuid";
 import session from "express-session";
-import { CreateUser } from "./db";
+import { CreateUser, GetUser, HashPassword } from "./db.js";
+
+//Session config
+const config = {
+  genid: (req) => uuid(),
+  secret: "keyboard cat",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
 
 const app = Express();
 app.use(cors());
+app.use(session(config));
 
 const PORT = 3001;
 let requests = 0;
@@ -29,11 +39,26 @@ app.post("/login", (req, res) => {
   const email = req.query.email;
   const password = req.query.password;
   requests++;
-  if (email == "test@test.com" && password == "123") {
-    res.send({ result: "success", email: "test@test.com", name: "David" });
-  } else {
-    res.send({ result: "fail" });
-  }
+
+  GetUser(email).then((r) => {
+    //if this email address is not taken
+    if (r.length === 1) {
+      //Save the user to the database
+      if (r[0].password === HashPassword(password)) {
+        //Passwords matched
+        console.log("Login success!");
+        res.send({ result: "success", email: email, name: r[0].name });
+      } else {
+        //Passwords did not match
+        console.log("Login failed! Invalid credentials");
+        res.send({ result: "fail", reason: "Invalid credentials" });
+      }
+    } else {
+      //Account does not exist
+      console.log("Login failed! Account does not exist.");
+      res.send({ result: "fail", reason: "Account does not exist" });
+    }
+  });
 });
 
 app.post("/register", (req, res) => {
@@ -41,14 +66,23 @@ app.post("/register", (req, res) => {
   const password = req.query.password;
   const name = req.query.name;
   const surname = req.query.surname;
-
-  CreateUser(anme,surname,email,password).then((r) => {
-    console.log(r);
-  });
-
   requests++;
-  console.log(req.query);
-  res.send({ result: "success", email: email, name: name });
+  //Step 1: Check if that email address already exists
+  //Step 2: If the email is not registered in the database we create it
+  //Step 3: If the account was created successfully we inform the user
+  GetUser(email).then((r) => {
+    //if this email address is not taken
+    if (r.length === 0) {
+      //Save the user to the database
+      CreateUser(name, surname, email, password)
+        .then((r) => {
+          res.send({ result: "success", email: email, name: name });
+        })
+        .catch((e) => console.log(e));
+    } else {
+      res.send({ result: "fail", reason: "Account already exists" });
+    }
+  });
 });
 
 //console.log(secretToken);
